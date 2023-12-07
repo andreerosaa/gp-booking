57c14a563842ea3980e65c0ca661606b457cdc45
andreerosaa <andrefsantosrosa@gmail.com>
2023-12-07 08:52:07 +0000

    adding events

--- src/SessionCalendar.js
+++ src/SessionCalendar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
@@ -9,20 +9,69 @@ const today = new Date();
 const localizer = momentLocalizer(moment);
 
 const SessionCalendar = () => {
-	const [sessions, setSessions] = useState([
-		{
-			id: 1,
-			title: 'Available',
-			start: new Date(2023, 11, 6, 9, 0, 0),
-			end: new Date(2023, 11, 6, 10, 0, 0),
-		},
-		// Add more session objects as needed
-	]);
-
 	const [userBookings, setUserBookings] = useState([]);
+	const [sessions, setSessions] = useState([]);
 	const [selectedSession, setSelectedSession] = useState(null);
 	const [modalIsOpen, setModalIsOpen] = useState(false);
 	const [userData, setUserData] = useState({ name: '', phoneNumber: '' });
+
+	// Function to generate recurring sessions
+	const generateRecurringSessions = () => {
+		const recurringSessions = [];
+		const startTime = moment().set({
+			hour: 8,
+			minute: 40,
+			second: 0,
+			millisecond: 0,
+		});
+		const lunchStartTime = moment().set({
+			hour: 12,
+			minute: 0,
+			second: 0,
+			millisecond: 0,
+		});
+		const lunchEndTime = moment().set({
+			hour: 13,
+			minute: 0,
+			second: 0,
+			millisecond: 0,
+		});
+
+		while (startTime.isBefore(lunchStartTime)) {
+			const endTime = startTime.clone().add(1, 'hour');
+
+			recurringSessions.push({
+				id: recurringSessions.length + 1,
+				title: 'Available',
+				start: startTime.toDate(),
+				end: endTime.toDate(),
+			});
+
+			startTime.add(70, 'minutes');
+		}
+
+		// Restart logic at 13:00
+		startTime.set({ hour: 13, minute: 0 });
+
+		while (startTime.isBefore(lunchStartTime)) {
+			const endTime = startTime.clone().add(1, 'hour');
+
+			recurringSessions.push({
+				id: recurringSessions.length + 1,
+				title: 'Available',
+				start: startTime.toDate(),
+				end: endTime.toDate(),
+			});
+
+			startTime.add(10, 'minutes');
+		}
+
+		setSessions(recurringSessions);
+	};
+
+	useEffect(() => {
+		generateRecurringSessions();
+	}, []);
 
 	const handleBooking = (event) => {
 		const clickedSession = sessions.find((session) => session.id === event.id);
@@ -55,8 +104,10 @@ const SessionCalendar = () => {
 					new Date(today.getFullYear(), today.getMonth(), today.getDate(), 8)
 				}
 				max={
-					new Date(today.getFullYear(), today.getMonth(), today.getDate(), 17)
+					new Date(today.getFullYear(), today.getMonth(), today.getDate(), 20)
 				}
+				defaultView='week'
+				views={['week', 'day']}
 				localizer={localizer}
 				events={sessions}
 				startAccessor='start'
